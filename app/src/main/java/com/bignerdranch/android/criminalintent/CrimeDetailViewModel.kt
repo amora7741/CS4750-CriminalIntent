363e5025bbb0512fd5f1e52f5e8b787a23128985
amora7741 <mora7741@gmail.com>
2023-09-23 21:07:22 -0700

    Allow changes on crime detail page

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailViewModel.kt
@@ -3,9 +3,11 @@ package com.bignerdranch.android.criminalintent
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import androidx.room.Update
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.util.UUID
 
@@ -18,6 +20,12 @@ class CrimeDetailViewModel(crimeId: UUID) : ViewModel(){
     init {
         viewModelScope.launch {
             _crime.value = crimeRepository.getCrime(crimeId)
+        }
+    }
+
+    fun updateCrime(onUpdate: (Crime) -> Crime) {
+        _crime.update { oldCrime ->
+            oldCrime?.let { onUpdate(it) }
         }
     }
 }
