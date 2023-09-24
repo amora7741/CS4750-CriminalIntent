44d3b0c1059682c87ee699c052e994b97121025a
amora7741 <mora7741@gmail.com>
2023-09-23 18:44:29 -0700

    Added database package

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Date
 import java.util.UUID
 
-data class Crime(val id: UUID, val title: String, val date: Date, val isSolved: Boolean)
+@Entity
+data class Crime(@PrimaryKey val id: UUID, val title: String, val date: Date, val isSolved: Boolean)
 
