0320af95d137a7c8143891c786fc580c280ea494
amora7741 <mora7741@gmail.com>
2023-09-24 13:53:10 -0700

    Removed preexisting database, new crimes added by user

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -15,7 +15,7 @@ class CrimeRepository private constructor(context: Context, private val coroutin
         context.applicationContext,
         CrimeDatabase::class.java,
         DATABASE_NAME
-    ).createFromAsset(DATABASE_NAME).build()
+    ).build()
 
     fun getCrimes(): Flow<List<Crime>> = database.crimeDao().getCrimes()
     suspend fun getCrime(id: UUID): Crime = database.crimeDao().getCrime(id)
@@ -24,6 +24,10 @@ class CrimeRepository private constructor(context: Context, private val coroutin
         coroutineScope.launch {
             database.crimeDao().updateCrime(crime)
         }
+    }
+
+    suspend fun addCrime(crime: Crime) {
+        database.crimeDao().addCrime(crime)
     }
     companion object {
         private var INSTANCE: CrimeRepository? = null
